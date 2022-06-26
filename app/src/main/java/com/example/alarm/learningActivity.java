a9817bef3824dcdd03866f568b27d8e3aee1b521
LAPTOP-0CL1GMG9\ghkim <ghkim7319@gmail.com>
2022-06-26 20:33:10 +0900

    alarm

---
+++ app/src/main/java/com/example/alarm/learningActivity.java
@@ -0,0 +1,15 @@
+package com.example.alarm;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class learningActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.activity_learning);
+    }
+
+}
